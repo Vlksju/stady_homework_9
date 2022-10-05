a4c61e4483550c2fdac4977505a573276f3e9eb1
Vlksju <xenya.vol2017@yandex.ru>
2022-10-05 22:19:46 +0500

    Solved task_66

---
+++ task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа, которая выводит сумму чисел в промежутке от M до N");
+
+Console.Write("Введите N - наименьшее ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите M - наибольшее ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int N, int M)
+{
+    if (M == N) return N;
+    
+    return M + SumNumbers (N, M-1);
+    
+}
+
+Console.WriteLine($"Сумма всех чисел = {SumNumbers(numberN, numberM)}");
