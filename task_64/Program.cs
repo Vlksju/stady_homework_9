e148940940a1a5919831e596139771285c48f71a
Vlksju <xenya.vol2017@yandex.ru>
2022-10-05 21:39:02 +0500

    Solved task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа, которая выводит все натуральные числа от N до 1");
+
+Console.WriteLine("Введите N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersToN(number);
+
+void PrintNumbersToN(int N)
+{
+    if (N < 1) return;
+    Console.Write(N + "");
+    PrintNumbersToN(N - 1);
+    
+}
